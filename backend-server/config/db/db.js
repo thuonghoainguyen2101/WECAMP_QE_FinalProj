a69b61cea7e5e338d8410c4b33b9511353ab539d
Thuong Nguyen <thaonhucute2001@gmail.com>
2022-07-16 15:00:45 +0700

    connect to mongodb

--- backend-server/config/db/db.js
+++ backend-server/config/db/db.js
@@ -1,20 +1,38 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-async function connectDB(){
-    const url = 'mongodb://localhost:27017/shop'
-    // const url = 'mongodb+srv://CaoKhaHieu:<CaoKhaHieu>@cluster0.r9hva.mongodb.net/shop?retryWrites=true&w=majority'
+async function connectDB() {
+    // const url = 'mongodb://localhost:27017/shop'
+    const url = process.env.DATABASE_URL;
+    // const url =
+    //     "mongodb+srv://CaoKhaHieu:<CaoKhaHieu>@cluster0.r9hva.mongodb.net/shop?retryWrites=true&w=majority";
+
     try {
         await mongoose.connect(url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
-        })
-        console.log("connected to db")
+        });
+        console.log("connected to db ");
+
+        // show all the collections
+        const collections = await mongoose.connection.db.collections();
+
+        // show all the data of each collection
+        for (let collection of collections) {
+            console.log(collection.collectionName);
+            const data = await collection.find({}).toArray();
+            console.log(data);
+        }
+
+        // a function to get sum of 2 number
+        // const sum = (a, b) => a + b;
+        // console.log(sum(1, 2));
+        
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
--- backend-server/models/ProductModel.js
+++ backend-server/models/ProductModel.js
@@ -1,62 +1,65 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const reviewProduct = new Schema({
-  name: {type: String},
-  comment: {type: String},
-  star: {type: Number},
-},{
-  timestamps: true,
-})
+const reviewProduct = new Schema(
+    {
+        name: { type: String },
+        comment: { type: String },
+        star: { type: Number },
+    },
+    {
+        timestamps: true,
+    }
+);
 
 const replieCommentProduct = new Schema({
-  content: {type: String},
-  isAdmin: Boolean,
-  nameUser: {type: String},
-  byUser: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-})
+    content: { type: String },
+    isAdmin: Boolean,
+    nameUser: { type: String },
+    byUser: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
 
 const commentProduct = new Schema({
-  author: {type: String},
-  status: String,
-  isAdmin: Boolean,
-  avatar: {type: String},
-  content: {type:String},
-  byUser: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  replies: [replieCommentProduct]
-})
+    author: { type: String },
+    status: String,
+    isAdmin: Boolean,
+    avatar: { type: String },
+    content: { type: String },
+    byUser: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    replies: [replieCommentProduct],
+});
 
 const Product = new Schema(
-  {
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    salePrice: { type: Number, required: true },
-    type: { type: String, required: true },
-    image: { type: String },
-    amount: Number,
-    cloudinary_id: { type: String },
+    {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        salePrice: { type: Number, required: true },
+        type: { type: String, required: true },
+        image: { type: String },
+        amount: Number,
+        cloudinary_id: { type: String },
 
-    rating: { type: Number },
-    numReviews: { type: Number },
-    blog: String,
+        rating: { type: Number },
+        numReviews: { type: Number },
+        blog: String,
 
-    reviews: [reviewProduct],
-    comments: [commentProduct],
+        reviews: [reviewProduct],
+        comments: [commentProduct],
 
-    os: String,
-    ram: String,
-    battery: String,
-    rom: String,
-    camera: String,
-    special: String,
-    design: String,
-    screen: String,
-  },
-  {
-    timestamps: true,
-  }
+        os: String,
+        ram: String,
+        battery: String,
+        rom: String,
+        camera: String,
+        special: String,
+        design: String,
+        screen: String,
+    },
+    {
+        timestamps: true,
+    }
 );
-Product.index({name: 'text'});
+Product.index({ name: "text" });
 
-export const ProductModel = mongoose.model("Product", Product)
+export const ProductModel = mongoose.model("Product", Product);
