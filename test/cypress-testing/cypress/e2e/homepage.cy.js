43562b3c0f1009a7ff545bb3fcc96d70a04a8543
trmnhi182 <tr.mnhi182@gmail.com>
2022-08-06 19:17:01 +0700

    e

--- test/cypress-testing/cypress/e2e/homepage.cy.js
+++ test/cypress-testing/cypress/e2e/homepage.cy.js
@@ -1,91 +1,76 @@
 /// <reference types="cypress" />
 
-// [ ] Verify that all of the elements are visible and working properly
-// [ ] Verify that all the text – logo, product, category name, price, and product description are clearly visible.
-// [ ] Verify that all the images – product and banner are clearly visible.
-// [ ] Verify that the search box has properly placeholder and icon
-// [ ] Verify that the system should redirect user to the search page
-// [ ] Verify that the Logo should redirect user to home page
-// [ ] Verify that the "Trang Chủ" should redirect user to Home page
-// [ ] Verify that the "Sản phẩm" should direct user to product page
-// [ ] Verify that the "Tài khoản" display 2 options "Đăng kí" and "Đăng nhập"
-// [ ] Verfiy that the system should redirect user to register page when user selects "Đăng kí"
-// [ ] Verfiy that the system should redirect user to login page when user selects "Đăng nhập"
-// [ ] Verify that the Cart icon will redirect user to cart page
-// [ ] Verify that the Cart icon will display number of items in the cart
-// [ ] Verify the image carousel is auto scolling
-// [ ] Verify the carousel is able to scroll to next image when it is hovered
-// [ ] Verify the carousel able to render the same way in different browsers and different screen resolutions
-// [ ] Verify the carousel <, > is displayed correctly when it is hovered
-// [ ] Verify the carousel <,> changes the slide correctly
-// [ ] Verify the carousel is able to click on and go to the correct page
-// [ ] Verify the content of carousel display clearly
+const homepagePage = require("../pages/homepagePage");
 
-// [ ] Verify that all the products in Homepage are redirecting to the correct product.
-// [ ] Verify that the "Mua Ngay" button can add the corresponding product to user's cart with the quantity is 1.
 
 describe("Homepage", () => {
     beforeEach(() => {
         cy.visit("/");
     });
 
-    xit("Verify that all of the elements are visible and working properly");
 
-    xit(
-        "Verify that all the text – logo, product, category name, price, and product description are clearly visible."
-    );
+    //Happy case - should pass
+    it('01 - Verify Logo on menu bar redirect user to correct page', () =>{
 
-    xit("Verify that all the images – product and banner are clearly visible.");
+        // const pages = ['']        
+        cy.get(homepagePage.logo).click();
+        cy.url().should("include", "/");
+        cy.go('back');
 
-    xit("Verify that the search box has properly placeholder and icon");
+        cy.get(homepagePage.searchIcon).click();
+        cy.url().should("include", "/search");
+        cy.go('back');
 
-    xit("Verify that the system should redirect user to the search page");
+        cy.get(homepagePage.homepageOption).click();
+        cy.url().should("include", "/");
+        cy.go('back');
 
-    xit("Verify that the Logo should redirect user to home page");
+        cy.get(homepagePage.productOption).click();
+        cy.url().should("include", "/product");
+        cy.go('back');
 
-    xit('Verify that the "Trang Chủ" should redirect user to Home page');
+        cy.get(homepagePage.cartOption).click();
+        cy.url().should("include", "/cart");
+        cy.go('back');
 
-    xit('Verify that the "Sản phẩm" should direct user to product page');
+        cy.get(homepagePage.accountOption).click();
+        cy.get(homepagePage.loginOption).click();
+        cy.url().should("include", "/login");
+        cy.go('back');
 
-    xit(
-        'Verify that the "Tài khoản" display 2 options "Đăng kí" and "Đăng nhập"'
-    );
+        cy.get(homepagePage.accountOption).click();
+        cy.get(homepagePage.registerOption).click();
+        cy.url().should("include", "/register");
+        cy.go('back');
+    });
 
-    xit(
-        'Verify that the system should redirect user to register page when user selects "Đăng kí"'
-    );
+    
+    it('02 - Verify the Cart Icon display number of items in cart and "Mua ngay" button', () =>{
+        //Visit page
+        //Scroll to view product
+        //Click on "Mua ngay" button on a product randomly
+        //Check cart icon if product is added
 
-    xit(
-        'Verify that the system should redirect user to login page when user selects "Đăng nhập"'
-    );
+    });
 
-    xit("Verify that the Cart icon will redirect user to cart page");
+    
+    it.only('03 - Prev and Next buttons on crousel display when mouse hover', () =>{
+        cy.get(homepagePage.carouselLeft).trigger('mouseover');
+        cy.get(homepagePage.nextButton).should('be.visible');
+    })
 
-    xit("Verify that the Cart icon will display number of items in the cart");
+    
+    it('04 - Change images correctly', () =>{
 
-    xit("Verify the image carousel is auto scolling");
+    })
 
-    xit(
-        "Verify the carousel is able to scroll to next image when it is hovered"
-    );
+    it('05 - Redirect to product\'s detail page', () =>{
+        
+    })
 
-    xit(
-        "Verify the carousel is able to render the same way in different browsers and different screen resolutions"
-    );
+    //User should not able to see chat icon when in anonymous mode
+    it('06 - Verify user in anonymous mode', () =>{
 
-    xit("Verify the carousel <, > is displayed correctly when it is hovered");
+    })
 
-    xit("Verify the carousel <,> changes the slide correctly");
-
-    xit("Verify the carousel is able to click on and go to the correct page");
-
-    xit("Verify the content of carousel display clearly");
-
-    xit(
-        "Verify that all the products in Homepage are redirecting to the correct product."
-    );
-
-    xit(
-        'Verify that the "Mua Ngay" button can add the corresponding product to user\'s cart with the quantity is 1.'
-    );
 });
---
+++ test/cypress-testing/cypress/pages/homepagePage.js
@@ -0,0 +1,38 @@
+class homePage{
+    constructor(){
+        //header
+        this.logo = "span > a"; 
+        this.searchBar = '.search';
+        this.searchIcon ="form > .anticon > svg"
+        this.homepageOption = "#hidden > li:nth-child(1) > a";
+        this.productOption = "#hidden > li:nth-child(2) > a";
+        this.accountOption = "#hidden > li:nth-child(3) > a";
+        this.cartOption = "#hidden > li:nth-child(4) > a";
+        this.loginOption = ".menu-drop > a:nth-child(2)";
+        this.registerOption = ".menu-drop > a:nth-child(1)";
+        this.numOfItems =".count";
+        
+
+        //Carousel
+        this.carouselLeft = ".carousel-left-slide";
+        this.slickSlide = ".slick-slider";
+        this.carouselRight =".carousel-right";
+        this.nextButton = "..next > .anticon > svg";
+        this.prevButton = ".prev";
+
+        //Product section
+        
+        
+
+        //Footer
+        this.aboutUs = ".footer-top-about > li:nth-child(1) > a";
+        this.blog = ".footer-top-about > li:nth-child(2) > a";
+        this.career = ".footer-top-about > li:nth-child(3) > a";
+        this.store = ".footer-top-about > li:nth-child(4) > a";
+
+        this.shippingMethods = ".footer-top-delivery > li:nth-child(1) > a";
+        this.payment = ".footer-top-delivery > li:nth-child(2) > a";
+        this.voucher = "footer-top-delivery > li:nth-child(3) > a";
+    }
+}
+module.exports = new homePage();
