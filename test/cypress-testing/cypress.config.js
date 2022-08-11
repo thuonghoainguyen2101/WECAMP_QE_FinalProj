5b1f98b053a52838dfb5f7a5cbc3106f146a203a
Thuong Nguyen <thaonhucute2001@gmail.com>
2022-08-12 04:42:05 +0700

    Test Detail Page Cypress

--- test/cypress-testing/cypress.config.js
+++ test/cypress-testing/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = {
             // implement node event listeners here
         },
         baseUrl: "http://localhost:3000",
+        backendUrl: "http://localhost:5000",
         chromeWebSecurity: false,
     },
 };
--- test/cypress-testing/cypress/pages/productDetailPage.js
+++ test/cypress-testing/cypress/pages/productDetailPage.js
@@ -10,14 +10,21 @@ class ProductDetailPage {
         this.cartOption = "#hidden > li:nth-child(4) > a";
         this.loginOption = ".menu-drop > a:nth-child(2)";
         this.registerOption = ".menu-drop > a:nth-child(1)";
-        this.numOfItems = ".count";
+        this.numberOfItems = ".count";
 
-        //content
+        //product information
         this.productName = "h2";
         this.productPrice = ".price";
         this.productSalePrice = ".saleprice";
         this.productImage = "img";
         this.productRating = ".star-average-num";
+
+        //product action
+        this.buyNowButton = ".detail-info-right-buy-now > a";
+        this.installmentButton1 =
+            ".detail-info-right-buy-installment > :nth-child(1)";
+        this.installmentButton2 =
+            ".detail-info-right-buy-installment > :nth-child(2)";
     }
 }
 
